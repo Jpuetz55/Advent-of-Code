2345ab7ed72b9055c5e3b6a2ee915233530043ed
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2023-12-02 17:16:10 -0600

    file open logic

--- Day 2 - 1 - Bag Game/grrs/src/main.rs
+++ Day 2 - 1 - Bag Game/grrs/src/main.rs
@@ -7,6 +7,22 @@ use std::path::Path;
 fn main()
 {
 
+    //crawl input by letter and computer with buffer?
+    //open file
+    
+    let path = Path::new("./params.txt");
+    let display = path.display();
+    let mut calib_params_file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
 
+    // write opened filed to string
+
+    let mut calib_params_string = String::new();
+    match calib_params_file.read_to_string(&mut calib_params_string) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => {}
+    }
 }
 
