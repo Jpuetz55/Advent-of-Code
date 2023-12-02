3a0e6f304e7578028cf4e691531cf0b1636b56f6
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2023-12-02 17:20:32 -0600

    set up loop

--- Day 2 - 1 - Bag Game/grrs/src/main.rs
+++ Day 2 - 1 - Bag Game/grrs/src/main.rs
@@ -6,23 +6,42 @@ use std::path::Path;
 
 fn main()
 {
+    const REDMAX: i32 = 12;
+    const GREENMAX: i32 = 13;
+    const BLUEMAX: i32 = 14;
 
     //crawl input by letter and computer with buffer?
     //open file
-    
+
     let path = Path::new("./params.txt");
     let display = path.display();
-    let mut calib_params_file = match File::open(&path) {
+    let mut game_params_file = match File::open(&path) {
         Err(why) => panic!("couldn't open {}: {}", display, why),
         Ok(file) => file,
     };
 
     // write opened filed to string
 
-    let mut calib_params_string = String::new();
-    match calib_params_file.read_to_string(&mut calib_params_string) {
+    let mut game_params_string = String::new();
+    match game_params_file.read_to_string(&mut game_params_string) {
         Err(why) => panic!("couldn't read {}: {}", display, why),
         Ok(_) => {}
     }
+
+    let game_params_split = calib_params_string.split("\n");
+
+    let collect_params = game_params_split.collect::<Vec<&str>>();
+
+    for item in collect_params {
+
+
+        //game counter
+        //
+
+
+
+    }
+
+
 }
 
