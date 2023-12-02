9778d47cdc4996a3ecf596da66526c6aedcd4797
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2023-12-02 12:16:44 -0600

    really close. must be one edge case I am missing

--- Day 1 - 2 - Trebuchet/grrs/src/main.rs
+++ Day 1 - 2 - Trebuchet/grrs/src/main.rs
@@ -5,163 +5,282 @@ use std::option;
 use std::path::Path;
 use circular_buffer::CircularBuffer;
 
-fn main() {
-    // open file with raw text
+fn main()
+{
 
-    let path = Path::new("./params.txt");
-    let display = path.display();
-    let mut calib_params_file = match File::open( & path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-            Ok(file) => file,
-    };
+        // open file with raw text
 
-    // write opened filed to string
+        let path = Path::new("./params.txt");
+        let display = path.display();
+        let mut calib_params_file = match File::open( & path) {
+            Err(why) => panic!("couldn't open {}: {}", display, why),
+                Ok(file) => file,
+        };
 
-    let mut calib_params_string = String::new();
-    match calib_params_file.read_to_string( & mut calib_params_string) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
-            Ok(_) => print!("{} contains:\n{}", display, calib_params_string),
-    }
-    // declare total to add calibration parameters to
+        // write opened filed to string
 
-    let mut total: u32 = 0;
+        let mut calib_params_string = String::new();
+        match calib_params_file.read_to_string( & mut calib_params_string) {
+            Err(why) => panic!("couldn't read {}: {}", display, why),
+                Ok(_) => print!("{} contains:\n{}", display, calib_params_string),
+        }
+        // declare total to add calibration parameters to
 
-    //declare variable to hold the final parameter calculation
+        let mut total: u32 = 0;
 
-    let mut add_first_last: u32 = 0;
+        //declare variable to hold the final parameter calculation
 
-    //counter
-    let mut i = 0;
+        let mut add_first_last: u32 = 0;
 
-    // Split raw calibration parameters by the new line and put them in a vector
+        //counter
+        let mut i = 0;
 
-    let calib_params_split = calib_params_string.split("\n");
+        // Split raw calibration parameters by the new line and put them in a vector
 
-    let collect_params = calib_params_split.collect:: < Vec < & str >> ();
+        //let calib_params_string = "9ftzbdsdkd9plrrtwo";
 
-    // Circular Buffer to check for word numbers
+        let calib_params_split = calib_params_string.split("\n");
 
-    // Initialize a new, empty circular buffer with a capacity of 5 elements
-    // when new value gets added, oldest value gets dropped and
-    // Ex buf = [1, 2, 3, 4, 5] -> buf.push_back(6) -> buf = [2, 3, 4, 5, 6]
-    let mut buf = CircularBuffer:: < 5,
-        char > ::new();
-    //buffers string representation
-    let mut current_string: String = String::new();
-    // Loop over each item in array
+        let collect_params = calib_params_split.collect:: < Vec < & str >> ();
 
-    for item in collect_params {
+        // Circular Buffer to check for word numbers
 
-        // declare temporary variable to hold the first_number and the last_number and set them to NULL 
-        let mut first_number: Option < u32 > = None;
-        let mut last_number: Option < u32 > = None;
-        // loop through characters in array item.
+        // Initialize a new, empty circular buffer with a capacity of 5 elements
+        // when new value gets added, oldest value gets dropped and
+        // Ex buf = [1, 2, 3, 4, 5] -> buf.push_back(6) -> buf = [2, 3, 4, 5, 6]
+        let mut buf = CircularBuffer:: < 5,
+            char > ::new();
+        // Loop over each item in array
 
-        //clear buffer
-        buf.drain(0..);
+        for item in collect_params {
 
-        for letter in item.chars() {
+            // declare temporary variable to hold the first_number and the last_number and set them to NULL 
+            let mut first_number: Option < u32 > = None;
+            let mut last_number: Option < u32 > = None;
+            // loop through characters in array item.
 
-            //push letter onto buffer if it isn't a new line
-            if letter != '\r' {
-                buf.push_back(letter);
-            }
-            
+            //clear buffer
+            let _ = buf.drain(0..);
 
-            //             
-                match current_string.as_str() {
-                     "zero" =>
-                        if first_number.is_none() {
-                            first_number = Some(0);
-                        }
-                        else {
-                            last_number = Some(0);
-                        },
-                    "one" =>
+            for letter in item.chars() {
+
+                //push letter onto buffer if it isn't a new line
+                if letter != '\r' {
+                    buf.push_back(letter);
+                }
+                if buf.len() > 2 {
+                    let buf3 = String::from_iter(buf.range(0..3));
+                    if buf3 == "one" {
                         if first_number.is_none() {
                             first_number = Some(1);
                         }
-                        else {
-                            last_number = Some(1);
-                        },
-                    "two" =>
+                        last_number = Some(1);
+                    }
+                    if buf3 == "two" {
                         if first_number.is_none() {
                             first_number = Some(2);
-                        } else {
-                            last_number = Some(2);
-                    },
-                    "three" =>
-                        if first_number.is_none() {
-                            first_number = Some(3);
-                        } else {
-                            last_number = Some(3);
-                        },
-                    "four" =>
-                        if first_number.is_none() {
-                            first_number = Some(4);
-                        } else {
-                            last_number = Some(4);
-                        },
-                    "five" =>
-                        if first_number.is_none() {
-                            first_number = Some(5);
-                        } else {
-                            last_number = Some(5);
-                        },
-                    "six" =>
+                        }
+                        last_number = Some(2);
+                    }
+
+                    if buf3 == "six" {
                         if first_number.is_none() {
                             first_number = Some(6);
-                        } else {
-                            last_number = Some(6);
-                        },
-                    "seven" =>
+                        }
+                        last_number = Some(6);
+                    }
+                }
+
+                if buf.len() > 3 {
+                    let buf4 = String::from_iter(buf.range(0..4));
+                    if buf4 == "zero" {
                         if first_number.is_none() {
-                            first_number = Some(7);
-                        } else {
-                            last_number = Some(7);
-                        },
-                    "eight" =>
+                            first_number = Some(0);
+                        }
+                        last_number = Some(0);
+                    }
+                    if buf4 == "four" {
                         if first_number.is_none() {
-                            first_number = Some(8);
-                        } else {
-                            last_number = Some(8);
-                        },
-                    "nine" =>
+                            first_number = Some(4);
+                        }
+                        last_number = Some(4);
+                    }
+                    if buf4 == "five" {
+                        if first_number.is_none() {
+                            first_number = Some(5);
+                        }
+                        last_number = Some(5);
+                    }
+                    if buf4 == "nine" {
                         if first_number.is_none() {
                             first_number = Some(9);
-                        } else {
-                            last_number = Some(9);
-                        },
-                    _ => {}
+                        }
+                        last_number = Some(9);
+                    }
+
+                }
+
+                if buf.len() > 4 {
+                    let buf5 = String::from_iter(buf.range(0..5));
+                    if buf5 == "three" {
+                        if first_number.is_none() {
+                            first_number = Some(3);
+                        }
+                        last_number = Some(3);
+                    }
+                    if buf5 == "seven" {
+                        if first_number.is_none() {
+                            first_number = Some(7);
+                        }
+                        last_number = Some(7);
+                    }
+                    if buf5 == "eight" {
+                        if first_number.is_none() {
+                            first_number = Some(8);
+                        }
+                        last_number = Some(8);
+                    }
+
+                }
+
+                // if letter.is_numeric() && first_number.is_empty then save to first_number and continue to next char
+                // if character == number
+                if letter.is_numeric() {
+                    //only set first_number when it it's none
+                    if first_number.is_none() {
+
+                        first_number = letter.to_digit(10);
+                    }
+                    //always set last_number when letter is numeric
+                    last_number = letter.to_digit(10);
+                };
+                //end loop -- when loop ends first_number should contain the first number and lastCharacter should contain the last number
             }
+            //buf should still be populated at the end of the loop
+            //To check for the case where a number word with a length smaller than the buffer punctuates an item:
 
-            // if letter.is_numeric() && first_number.is_empty then save to first_number and continue to next char
-            //is numerica and non numeric the only other posiblities?
-            print!("{}\n", current_string);
-            if letter.is_numeric() && first_number.is_none() {
-                first_number = letter.to_digit(10)
-            };
-            // if character == number && first_number !== NULL then save to last_number and continue 
+            //push char to buffer and check for 4 letter words at the end
+            if (!buf.get(3).is_none() &&
+                !buf.get(3).unwrap().is_numeric()) && (!buf.get(4).is_none() &&
+                !buf.get(4).unwrap().is_numeric()) 
+            {
+                buf.push_back('.');
+                if buf.len() > 2 {
+                    let buf3 = String::from_iter(buf.range(0..3));
+                    if buf3 == "one" {
+                        if first_number.is_none() {
+                            first_number = Some(1);
+                        }
+                        last_number = Some(1);
+                    }
+                    if buf3 == "two" {
+                        if first_number.is_none() {
+                            first_number = Some(2);
+                        }
+                        last_number = Some(2);
+                    }
 
-            if letter.is_numeric() && !first_number.is_none() {
-                last_number = letter.to_digit(10)
-            };
+                    if buf3 == "six" {
+                        if first_number.is_none() {
+                            first_number = Some(6);
+                        }
+                        last_number = Some(6);
+                    }
+                    if buf.len() > 3 {
+                        let buf4 = String::from_iter(buf.range(0..4));
+                        if buf4 == "zero" {
+                            if first_number.is_none() {
+                                first_number = Some(0);
+                            }
+                            last_number = Some(0);
+                        }
+                        if buf4 == "four" {
+                            if first_number.is_none() {
+                                first_number = Some(4);
+                            }
+                            last_number = Some(4);
+                        }
+                        if buf4 == "five" {
+                            if first_number.is_none() {
+                                first_number = Some(5);
+                            }
+                            last_number = Some(5);
+                        }
+                        if buf4 == "nine" {
+                            if first_number.is_none() {
+                                first_number = Some(9);
+                            }
+                            last_number = Some(9);
+                        }
+                    }
+                }
+                //push char to buffer and check for 3 letter words at the end
+                buf.push_back('.');
+                if buf.len() > 2 {
+                    let buf3 = String::from_iter(buf.range(0..3));
+                    if buf3 == "one" {
+                        if first_number.is_none() {
+                            first_number = Some(1);
+                        }
+                        last_number = Some(1);
+                    }
+                    if buf3 == "two" {
+                        if first_number.is_none() {
+                            first_number = Some(2);
+                        }
+                        last_number = Some(2);
+                    }
 
-            //end loop -- when loop ends first_number should contain the first number and lastCharacter should contain the last number
+                    if buf3 == "six" {
+                        if first_number.is_none() {
+                            first_number = Some(6);
+                        }
+                        last_number = Some(6);
+                    }
+                }
+                if buf.len() > 3 {
+                    let buf4 = String::from_iter(buf.range(0..4));
+                    if buf4 == "zero" {
+                        if first_number.is_none() {
+                            first_number = Some(0);
+                        }
+                        last_number = Some(0);
+                    }
+                    if buf4 == "four" {
+                        if first_number.is_none() {
+                            first_number = Some(4);
+                        }
+                        last_number = Some(4);
+                    }
+                    if buf4 == "five" {
+                        if first_number.is_none() {
+                            first_number = Some(5);
+                        }
+                        last_number = Some(5);
+                    }
+                    if buf4 == "nine" {
+                        if first_number.is_none() {
+                            first_number = Some(9);
+                        }
+                        last_number = Some(9);
+                    }
+
+                }
+            }
+        
+            // calculate parameters
+            //(first number * 10) + finalNumber?
+            add_first_last = (first_number.unwrap() * 10) + last_number.unwrap();
+            //add them to total
+            total += add_first_last;
+            //set back to zero for next loop
+            print!("\t\t{} : {:?}, {:?} : {:?}", item, first_number.unwrap(), last_number.unwrap(), add_first_last);
+            //print!("\t{}{:?}\n", i, buf.range(0..));
+            print!("\t{}\n", item);
+            add_first_last = 0;
+            i += 1;
+            //end loop
         }
-
-        // calculate parameters
-        //(first number * 10) + finalNumber?
-        add_first_last = (first_number.unwrap() * 10) + last_number.unwrap();
-        //add them to total
-        total += add_first_last;
-        //set back to zero for next loop
-        //print!("{} : {:?}, {:?} : {:?}\n", item, first_number.unwrap(), last_number.unwrap(), add_first_last);
-        print!("{}{:?}\n", i, buf.range(0..));
-        add_first_last = 0;
-        i += 1;
-        //end loop    
-    }
-    print!("Total is : {:?}", total); // answer is 52974    
-    // function returns the value of total 
+            print!("Total is : {:?}", total); //
 }
+
