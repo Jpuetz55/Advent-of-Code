16e966305ce7249597ae4362204be6face547b73
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2023-12-03 11:49:56 -0600

    clean

--- Day 3 - 1 - Gondola Lift/grrs/src/main.rs
+++ Day 3 - 1 - Gondola Lift/grrs/src/main.rs
@@ -4,21 +4,25 @@
 fn main() {
 
     //have to think of each number being in the center of a 5x5 matrix
-
+    //all 3 digit number
     // EX.
 
     // X X X X X
     // x 1 1 1 x
     // x x x x x
+
     //if there is anything other than a period in the x cells, then the number in the middle is
     //a part number and should be added to the sum
+
     //all lines are equal length, so I should be able to calculate and check the location of each cell
     //in the 5 x 5 matrix by moving forward or backward in the file a set amount for each position
+
     //EX. with the very center cell considered as the current anchor.
+    
     // (i-(backward)) (i-(backward + 1)) (i-(backward + 2)) (i-(backward + 3)) (i-(backward + 4))
     //      (i-2)              1                  1                   1                (i+2)
     // (i+(forward)) (i+(forward + 1)) (i+(forward + 2)) (i+(forward + 3)) (i+(forward + 4))
 
-    
+
     println!("Hello, world!");
 }
