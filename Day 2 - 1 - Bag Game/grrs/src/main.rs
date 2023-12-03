03de63186bf43b23953ead9d829d887ced017b3f
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2023-12-03 10:23:21 -0600

    cleanup/refactor

--- Day 2 - 1 - Bag Game/grrs/src/main.rs
+++ Day 2 - 1 - Bag Game/grrs/src/main.rs
@@ -52,37 +52,43 @@ fn main()
 
     let games = game_params_split.collect::<Vec<&str>>();
 
-    print!("{:?}\n", games);
+    // print!("{:?}\n", games);
 
     //game counter
     let mut game_count: u32 = 1;
     //game total
     let mut game_total: u32 = 0;
-    //iterator to help read input
+    //iterators to help read input/debug
     let mut i: u32 = 0;   //letter iterator
     let mut j: u32 = 0;   //game iterator
 
+    let mut temp_color_count: u32 = 0;  //hold total value for digit computation
+    //red counter per game
+    let mut red_count: u32 = 0;
+    //green counter per game
+    let mut green_count: u32 = 0;
+    //blue counter per game
+    let mut blue_count: u32 = 0;
+    //signal failure from letter loop to game loop
+    let mut fail_flag: u32 = 0;
+
     for game in games {
-        let mut temp_color_count: u32 = 0;  //hold total value for digit computation
-        //red counter
-        let mut red_count: u32 = 0;
-        //green counter
-        let mut green_count: u32 = 0;
-        //blue counter
-        let mut blue_count: u32 = 0;
-        //signal failure from letter loop to game loop
-        let mut fail_flag: u32 = 0;
         
+        
+        red_count = 0;
+        green_count= 0;
+        blue_count = 0;
+
         for letter in game.chars() {
             if i >= 7
             {
                 if letter.is_numeric() 
                 {   
-                     temp_color_count = 0;               
+                    temp_color_count = 0;               
                     digits.push(letter.to_digit(10).unwrap()); //add to vec
                     if !game.chars().nth((i + 1).try_into().unwrap()).unwrap().is_digit(10) 
                     { //if next is not a digit i.e. ->  ' '. digit end. compute
-                        let mut mult_casc = 1; 
+                        let mut mult_casc = 1;   //iterate backwards on vec, aka, starting from the ones and multiply multiplication factor by 10 for each digit. add results together
                         for &element in digits.iter().rev() {
                             let temp = element * mult_casc;
                             //multiply by mult_cascade
@@ -116,22 +122,23 @@ fn main()
             //need to make it so if one color fails, it stops entering this loop and adding the game count to the total      
             if blue_count > BLUEMAX {
                 fail_flag = 1;
-                print!("Blue Failed {}", game_count);
+                print!("Blue Failed {}----", game_count);
                 break;
             }
             if green_count > GREENMAX {                
                 fail_flag = 1;
-                print!("Green Failed {}", game_count);
+                print!("Green Failed {}----", game_count);
                 break;
             }
             if red_count > REDMAX {
                 fail_flag = 1;
-                print!("Red Failed {}", game_count);
+                print!("Red Failed {}----", game_count);
                 break;
             }   
             i += 1;            
         }
         //duplicate some end loop logic that needs to execute on both success and failure
+        // on failure
         if fail_flag == 1 {
             fail_flag = 0;
             j += 1;                                                           
@@ -139,18 +146,19 @@ fn main()
             //set i back to zero for next game
             i = 0;
             game_count += 1;
-            break;
         }
-        //only on success
-        print!("Success --- Game Total:  {}\t", game_total);
-        game_total += game_count;   
-        j += 1;                                                           
-        print!("end loop {}\n", j);       
-        //set i back to zero for next game
-        i = 0;
-        game_count += 1;   
+        //on success
+        else { 
+            print!("----Game Total Previous:  {}\t", game_total);          
+            game_total += game_count;
+            print!("Success --- Game Total:  {}\t", game_total);   
+            j += 1;                                                           
+            print!("end loop {}\n", j);       
+            //set i back to zero for next game
+            i = 0;
+            game_count += 1; 
+        }         
     }
-
     print!("{}", game_total);
 }
 
