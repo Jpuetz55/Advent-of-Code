cdd4f28f2c0f8d1de442d33808a288ab679412a8
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2023-12-03 11:23:23 -0600

    first attempt

--- Day 2 - 2 - Bag Game/grrs/src/main.rs
+++ Day 2 - 2 - Bag Game/grrs/src/main.rs
@@ -59,34 +59,33 @@ fn main()
     let mut game_total: u32 = 0;
     //iterators to help read input/debug
     let mut i: u32 = 0;   //letter iterator
-    let mut j: u32 = 0;   //game iterator
+    let mut j: u32 = 1;   //game iterator
 
     let mut temp_color_count: u32 = 0;  //hold total value for digit computation
-    //red counter per game
-    let mut red_count: u32 = 0;
-    //green counter per game
-    let mut green_count: u32 = 0;
-    //blue counter per game
-    let mut blue_count: u32 = 0;
+    //red max counter per game
+    let mut red_max_count: u32 = 0;
+    //green max counter per game
+    let mut green_max_count: u32 = 0;
+    //blue max counter per game
+    let mut blue_max_count: u32 = 0;
     //signal failure from letter loop to game loop
     let mut fail_flag: u32 = 0;
 
     for game in games {
         
         
-        red_count = 0;
-        green_count= 0;
-        blue_count = 0;
+        red_max_count = 0;
+        green_max_count= 0;
+        blue_max_count = 0;
 
         for letter in game.chars() {
             if i >= 7
             {
                 if letter.is_numeric() 
-                {   
-                    temp_color_count = 0;               
+                {                                      
                     digits.push(letter.to_digit(10).unwrap()); //add to vec
-                    if !game.chars().nth((i + 1).try_into().unwrap()).unwrap().is_digit(10) 
-                    { //if next is not a digit i.e. ->  ' '. digit end. compute
+                    if !game.chars().nth((i + 1).try_into().unwrap()).unwrap().is_digit(10) //next character sin't a digit. we have all digits now.
+                    { 
                         let mut mult_casc = 1;   //iterate backwards on vec, aka, starting from the ones and multiply multiplication factor by 10 for each digit. add results together
                         for &element in digits.iter().rev() {
                             let temp = element * mult_casc;
@@ -101,62 +100,85 @@ fn main()
                         //skip forward 2, get letter (r, b, g)
                         let color_char: char = game.chars().nth((i + 2).try_into().unwrap()).unwrap();
 
+                        // check max with new value obtained above
                         match color_char {
                             'r' => {
-                                red_count += temp_color_count
+                                if temp_color_count > red_max_count {
+                                    red_max_count = temp_color_count;
+                                }
+                                
                             }
                             'g' => {
-                                green_count += temp_color_count
+                                if temp_color_count > green_max_count {
+                                    green_max_count = temp_color_count;
+                                }
                             }
                             'b' => {
-                                blue_count += temp_color_count
+                                if temp_color_count > blue_max_count {
+                                    blue_max_count = temp_color_count;
+                                }
                             },
                             _ => {}
                         } 
-                        digits.clear();                     
+                        //clear for next number
+                        digits.clear();  
+                        //reset for next number
+                        temp_color_count = 0;                   
                     }
                 }                           
             }
             //check failure condition
             //need to make it so if one color fails, it stops entering this loop and adding the game count to the total      
-            if blue_count > BLUEMAX {
-                fail_flag = 1;
-                print!("Blue Failed {}----", game_count);
-                break;
-            }
-            if green_count > GREENMAX {                
-                fail_flag = 1;
-                print!("Green Failed {}----", game_count);
-                break;
-            }
-            if red_count > REDMAX {
-                fail_flag = 1;
-                print!("Red Failed {}----", game_count);
-                break;
-            }   
-            i += 1;            
-        }
+            // if blue_max_count > BLUEMAX {
+            //     fail_flag = 1;
+            //     print!("Blue Failed {}----", game_count);
+            //     break;
+            // }
+            // if green_max_count > GREENMAX {                
+            //     fail_flag = 1;
+            //     print!("Green Failed {}----", game_count);
+            //     break;
+            // }
+            // if red_max_count > REDMAX {
+            //     fail_flag = 1;
+            //     print!("Red Failed {}----", game_count);
+            //     break;
+            // } 
+            i += 1;              
+        } 
+        //have all max color counts for game now
+
+        // multiply them together with result added to running total 
+        print!("Game Total Prev {}----", game_total);
+        game_total += red_max_count * green_max_count * blue_max_count;
+        print!("Game Total Prev + {} = {}----", red_max_count * green_max_count * blue_max_count, game_total);
+        print!("Blue Max {}----", blue_max_count);
+        print!("Green Max {}----", green_max_count);
+        print!("Red Max {}----", red_max_count);
+        print!("end loop {}\n", j);
+        j += 1;
+
         //duplicate some end loop logic that needs to execute on both success and failure
         // on failure
-        if fail_flag == 1 {
-            fail_flag = 0;
-            j += 1;                                                           
-            print!("end loop {}\n", j); 
-            //set i back to zero for next game
-            i = 0;
-            game_count += 1;
-        }
+        // if fail_flag == 1 {
+        //     fail_flag = 0;
+        //     j += 1;                                                           
+        //     print!("end loop {}\n", j); 
+        //     //set i back to zero for next game
+        //     i = 0;
+        //     game_count += 1;
+        // }
         //on success
-        else { 
-            print!("----Game Total Previous:  {}\t", game_total);          
-            game_total += game_count;
-            print!("Success --- Game Total:  {}\t", game_total);   
-            j += 1;                                                           
-            print!("end loop {}\n", j);       
-            //set i back to zero for next game
-            i = 0;
-            game_count += 1; 
-        }         
+        // else { 
+        //     print!("----Game Total Previous:  {}\t", game_total);          
+        //     game_total += game_count;
+        //     print!("Success --- Game Total:  {}\t", game_total);   
+        //     j += 1;                                                           
+        //     print!("end loop {}\n", j);       
+        //     //set i back to zero for next game
+        //     i = 0;
+        //     game_count += 1; 
+        // }         
     }
     print!("{}", game_total);
 }
