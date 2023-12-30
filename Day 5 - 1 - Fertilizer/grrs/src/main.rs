a902821a9bce04251f75dda6317fc3d31043cb1b
jpuetz55 <jpuetz55@gmail.com>
2023-12-29 18:07:10 -0600

    fix brackets

--- Day 5 - 1 - Fertilizer/grrs/src/main.rs
+++ Day 5 - 1 - Fertilizer/grrs/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+
 // Function to parse a single map and return a 2D array
 fn parse_map(map_str: &str) -> Vec<usize> {
     map_str
@@ -8,11 +9,13 @@ fn parse_map(map_str: &str) -> Vec<usize> {
         .map(|num_str| num_str.parse().unwrap())
         .collect()
 }
+
 // Function to parse the entire input and return a vector of 2D arrays
 fn parse_input(input: &str) -> Vec<Vec<usize>> {
     let mut maps = Vec::new();
     let input_to_vector: Vec<&str> = input.lines().collect();
     let mut current_map: Vec<usize> = Vec::new();
+
     for line in input_to_vector {
         if !line.is_empty() && line.chars().nth(0).unwrap().is_digit(10) {
             // Parse the line and add to the current map
@@ -23,12 +26,15 @@ fn parse_input(input: &str) -> Vec<Vec<usize>> {
             current_map.clear();
         }
     }
+
     // Add the last map if it's not empty
     if !current_map.is_empty() {
         maps.push(current_map);
     }
+
     maps
 }
+
 fn main() {
     // Read parameters from a file
     let path = Path::new("./params.txt");
@@ -37,28 +43,31 @@ fn main() {
         Err(why) => panic!("couldn't open {}: {}", display, why),
         Ok(file) => file,
     };
+
     // Read the file content into a string
     let mut params_string = String::new();
     match params_file.read_to_string(&mut params_string) {
         Err(why) => panic!("couldn't read {}: {}", display, why),
         Ok(_) => {}
     }
-    //parse the seed numbers and put them in a vector
-    let first_line: Option<&str> = params_string.lines().next();
-    let mut seed_numbers: Vec<&str> = Vec::new();
 
-    let parts: Vec<&str> = first_line.unwrap().split(":").collect();
-    // Now you can use the parts vector for further processing
-    // ...
-    let seed_numbers = parts[1]
-        .split_whitespace()
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect::<Vec<usize>>();
+    // Parse the seed numbers and put them in a vector
+    let first_line: Option<&str> = params_string.lines().next();
+    let mut seed_numbers: Vec<usize> = Vec::new();
+
+    if let Some(line) = first_line {
+        let parts: Vec<&str> = line.split(":").collect();
+        seed_numbers = parts[1]
+            .split_whitespace()
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+    }
 
     println!("Seed Numbers: {:?}", seed_numbers);
 
     // Parse the input into a vector of 2D arrays
     let maps = parse_input(&params_string);
+
     // Iterate through all maps and print their information
     for (index, map) in maps.iter().enumerate() {
         // Iterate through lines within the current map
@@ -68,6 +77,7 @@ fn main() {
             // For the next two lines, we'll need to adjust depending on the length of each line
             let source_start = *line_values; // Example: Assuming the line has at least 2 values
             let range_length = *line_values; // Example: Assuming the line has at least 3 values
+
             // Print values from the current line
             println!(
                 "Map {}: Line {}: Destination Start: {}, Source Start: {}, Range Length: {}",
@@ -77,13 +87,14 @@ fn main() {
                 source_start,
                 range_length
             );
+
             // Starting at the first line of the map
             // Iterate through the next maps
-            //need to create a loop for the lines inside of each map
-            
-            //need a mut variable to store the initial seed number and have it
-            //update with the value derived from each iteration through a map
-            for seed in seed_numbers {
+            // Need to create a loop for the lines inside each map
+
+            // Need a mutable variable to store the initial seed number
+            // and have it update with the value derived from each iteration through a map
+            for seed in seed_numbers.iter() {
                 for (index, map) in maps.iter().enumerate() {
                     // Calculate the seed range start and end in the current map
                     // Access individual values in the line without dereferencing
@@ -93,13 +104,12 @@ fn main() {
                     let range_length = *line_values; // Example: Assuming the line has at least 3 values
 
                     // Check if the destination seed falls within the range
-                    if seed >= source_start && seed <= source_start + range_length {
+                    if seed >= &source_start && seed <= &(source_start + range_length) {
                         // Calculate the distance between the source seed in the current map and the destination seed
-                        let distance = seed - source_start;             
+                        let distance = seed - source_start;
 
-                            // Go straight to the next map
-                            break;
-                        }
+                        // Go straight to the next map
+                        break;
                     }
 
                     // If the destination seed is not found in the current map, go to the next line in the map
