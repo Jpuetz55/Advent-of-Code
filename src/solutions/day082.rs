e5f9b10638ed7dea0006b768ad0bcaedd1f142ab
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2024-01-22 23:30:50 -0600

    compiling

--- src/solutions/day082.rs
+++ src/solutions/day082.rs
@@ -7,10 +7,12 @@ pub enum PuzzleErr {
     #[error("Could not locate digit.")]
     NoDigits,
 }
+
 // Main puzzle-solving function
 pub fn puzzle(input_data: &str) -> Result<isize, PuzzleErr> {
     calculate_total(input_data)
 }
+
 // Function to calculate total winnings
 fn calculate_total(input_data: &str) -> Result<isize, PuzzleErr> {
     let mut choices_map: HashMap<&str, (&str, &str)> = HashMap::new();
@@ -20,11 +22,9 @@ fn calculate_total(input_data: &str) -> Result<isize, PuzzleErr> {
         .map(|s| s.trim())
         .collect();
 
-    let pattern = parts[0];
     let map = parts[1];
-    // hard set starting key
-    let start_key = "AAA";
 
+    //parse input and create hashmap to hold the key value pairs
     for line in map.lines() {
         let parts: Vec<&str> = line
             .split('=')
@@ -43,26 +43,62 @@ fn calculate_total(input_data: &str) -> Result<isize, PuzzleErr> {
         }
     }
 
-    let mut key = start_key;
-    //iterator for pattern
-    let mut i = 0;
-    let mut select_choice = "";
-
     let mut steps: isize = 0;
-    while key != "ZZZ" {
-        if i >= pattern.len() {
-            i = 0;
+
+    //get all key values from map wherein the key ends with an A
+
+    //intialize a vector to hold the key value pairs with the initial values (ends with A). select left first
+    let mut value_vec: Vec<&str> = choices_map
+        .iter()
+        .filter(|(key, _value)| key.ends_with("A"))
+        .map(|(key, value)| {
+            println!("Initial: key={}, value={:?}", key, value);
+            value.0
+        })
+        .collect();
+
+    //iterate through the values and end the function and return the step count when all the values end with a z
+    //initialize a boolean switch to right value
+    let mut bool_switch = false;
+
+    loop {
+        if value_vec.iter().all(|value| value.ends_with("Z")) {
+            break;
+        } else {
+            //populate new vec with all the values selected from the value_vec
+            //choose value 1 if bool_switch is true, value2 otherwise
+            if bool_switch {
+                println!("Switch is true");
+                value_vec = value_vec
+                    .iter()
+                    .map(|&value| {
+                        let new_value = choices_map.get(value).map_or(value, |(v, _)| {
+                            println!("Choosing left: value={}", v);
+                            v
+                        });
+                        new_value
+                    })
+                    .collect();
+            } else {
+                println!("Switch is false");
+                value_vec = value_vec
+                    .iter()
+                    .map(|&value| {
+                        let new_value = choices_map.get(value).map_or(value, |(_, v)| {
+                            println!("Choosing right: value={}", v);
+                            v
+                        });
+                        new_value
+                    })
+                    .collect();
+            }
+
+            //flip switch
+            bool_switch = !bool_switch;
+            steps += 1;
         }
-        steps += 1;
-        let (choice1, choice2) = choices_map.get(key).unwrap();
-        if pattern.chars().nth(i).unwrap() == 'L' {
-            select_choice = choice1;
-        } else if pattern.chars().nth(i).unwrap() == 'R' {
-            select_choice = choice2;
-        }
-        key = select_choice; // Fix here: use the choice directly as the new key
-        i += 1;
     }
+
     Ok(steps)
 }
 
