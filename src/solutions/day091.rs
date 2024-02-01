7dc525d71801a56a95c6668275e757ccfedc4ca7
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2024-01-31 22:49:22 -0600

    psuedocode

--- src/solutions/day091.rs
+++ src/solutions/day091.rs
@@ -15,6 +15,21 @@ pub fn puzzle(input_data: &str) -> Result<isize, PuzzleErr> {
 
 // Function to calculate total winnings
 fn calculate_total(input_data: &str) -> Result<isize, PuzzleErr> {
+    //for each line in input_data
+    //declare vecs Vec to store all number_vec's
+    //set start_vec from input_data
+    //set index to first item
+    //compare with next item
+    //push the difference to difference_vec (scope: loop)
+    //do this until the current_index is the last value in the start_vec (scope: loop)
+    //check to see if all the values are zero
+    //if false
+    //push start_vec to vecs
+    //restart the process with the difference_vec as the start_vec
+    //if true
+    //start at the last vec in vecs, get the value of the last index, add it to the last value in the next vec, repeat this process
+    //until the next vec is the first vec in vecs. add this value to running_total (scope:global)
+
     Ok(0 as isize)
 }
 
