783dbf2d9a3e1c893fd72f9986e035ba6314ee8b
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2024-02-03 17:00:18 -0600

    day 9-1 complete

--- src/lib.rs
+++ src/lib.rs
@@ -4,215 +4,215 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error<'a> {
-    #[error("Day not yet implemented: {}.", .0)] DayNotImplemented(&'a str),
+  #[error("Day not yet implemented: {}.", .0)] DayNotImplemented(&'a str),
 }
 
 pub fn run_day<'a>(data_dir: &'a str, day: &'a str) -> Result<(), Error<'a>> {
-    match day {
-        "011" => {
-            solutions::day011::main(data_dir);
-            Ok(())
-        }
-        "012" => {
-            solutions::day012::main(data_dir);
-            Ok(())
-        }
-        "021" => {
-            solutions::day021::main(data_dir);
-            Ok(())
-        }
-        "022" => {
-            solutions::day022::main(data_dir);
-            Ok(())
-        }
-        "031" => {
-            solutions::day031::main(data_dir);
-            Ok(())
-        }
-        "032" => {
-            solutions::day032::main(data_dir);
-            Ok(())
-        }
-        "041" => {
-            solutions::day041::main(data_dir);
-            Ok(())
-        }
-        "042" => {
-            solutions::day042::main(data_dir);
-            Ok(())
-        }
-        "051" => {
-            solutions::day051::main(data_dir);
-            Ok(())
-        }
-        "052" => {
-            solutions::day052::main(data_dir);
-            Ok(())
-        }
-        "061" => {
-            solutions::day061::main(data_dir);
-            Ok(())
-        }
-        "062" => {
-            solutions::day062::main(data_dir);
-            Ok(())
-        }
-        "071" => {
-            solutions::day071::main(data_dir);
-            Ok(())
-        }
-        "072" => {
-            solutions::day072::main(data_dir);
-            Ok(())
-        }
-        "081" => {
-            solutions::day081::main(data_dir);
-            Ok(())
-        }
-        "082" => {
-            solutions::day082::main(data_dir); // Fix the function call
-            Ok(())
-        }
-        // "091" => {
-        //     solutions::day091::main(data_dir);
-        //     Ok(())
-        // }
-        // "092" => {
-        //     solutions::day092::main(data_dir);
-        //     Ok(())
-        // }
-        // "101" => {
-        //     solutions::day101::main(data_dir);
-        //     Ok(())
-        // }
-        // "102" => {
-        //     solutions::day102::main(data_dir);
-        //     Ok(())
-        // }
-        // "111" => {
-        //     solutions::day111::main(data_dir);
-        //     Ok(())
-        // }
-        // "112" => {
-        //     solutions::day112::main(data_dir);
-        //     Ok(())
-        // }
-        // "121" => {
-        //     solutions::day121::main(data_dir);
-        //     Ok(())
-        // }
-        // "122" => {
-        //     solutions::day122::main(data_dir);
-        //     Ok(())
-        // }
-        // "131" => {
-        //     solutions::day131::main(data_dir);
-        //     Ok(())
-        // }
-        // "132" => {
-        //     solutions::day132::main(data_dir);
-        //     Ok(())
-        // }
-        // "141" => {
-        //     solutions::day141::main(data_dir);
-        //     Ok(())
-        // }
-        // "142" => {
-        //     solutions::day142::main(data_dir);
-        //     Ok(())
-        // }
-        // "151" => {
-        //     solutions::day151::main(data_dir);
-        //     Ok(())
-        // }
-        // "152" => {
-        //     solutions::day152::main(data_dir);
-        //     Ok(())
-        // }
-        // "161" => {
-        //     solutions::day161::main(data_dir);
-        //     Ok(())
-        // }
-        // "162" => {
-        //     solutions::day162::main(data_dir);
-        //     Ok(())
-        // }
-        // "171" => {
-        //     solutions::day171::main(data_dir);
-        //     Ok(())
-        // }
-        // "172" => {
-        //     solutions::day172::main(data_dir);
-        //     Ok(())
-        // }
-        // "181" => {
-        //     solutions::day181::main(data_dir);
-        //     Ok(())
-        // }
-        // "182" => {
-        //     solutions::day182::main(data_dir);
-        //     Ok(())
-        // }
-        // "191" => {
-        //     solutions::day191::main(data_dir);
-        //     Ok(())
-        // }
-        // "192" => {
-        //     solutions::day192::main(data_dir);
-        //     Ok(())
-        // }
-        // "201" => {
-        //     solutions::day201::main(data_dir);
-        //     Ok(())
-        // }
-        // "202" => {
-        //     solutions::day202::main(data_dir);
-        //     Ok(())
-        // }
-        // "211" => {
-        //     solutions::day211::main(data_dir);
-        //     Ok(())
-        // }
-        // "212" => {
-        //     solutions::day212::main(data_dir);
-        //     Ok(())
-        // }
-        // "221" => {
-        //     solutions::day221::main(data_dir);
-        //     Ok(())
-        // }
-        // "222" => {
-        //     solutions::day222::main(data_dir);
-        //     Ok(())
-        // }
-        // "231" => {
-        //     solutions::day231::main(data_dir);
-        //     Ok(())
-        // }
-        // "232" => {
-        //     solutions::day232::main(data_dir);
-        //     Ok(())
-        // }
-        // "241" => {
-        //     solutions::day241::main(data_dir);
-        //     Ok(())
-        // }
-        // "242" => {
-        //     solutions::day242::main(data_dir);
-        //     Ok(())
-        // }
-        // "251" => {
-        //     solutions::day251::main(data_dir);
-        //     Ok(())
-        // }
-        // "252" => {
-        //     solutions::day252::main(data_dir);
-        //     Ok(())
-        // }
-
-        // <-- INSERT NEW DAY HERE -->
-        _ => Err(Error::DayNotImplemented(day)),
+  match day {
+    "011" => {
+      solutions::day011::main(data_dir);
+      Ok(())
     }
+    "012" => {
+      solutions::day012::main(data_dir);
+      Ok(())
+    }
+    "021" => {
+      solutions::day021::main(data_dir);
+      Ok(())
+    }
+    "022" => {
+      solutions::day022::main(data_dir);
+      Ok(())
+    }
+    "031" => {
+      solutions::day031::main(data_dir);
+      Ok(())
+    }
+    "032" => {
+      solutions::day032::main(data_dir);
+      Ok(())
+    }
+    "041" => {
+      solutions::day041::main(data_dir);
+      Ok(())
+    }
+    "042" => {
+      solutions::day042::main(data_dir);
+      Ok(())
+    }
+    "051" => {
+      solutions::day051::main(data_dir);
+      Ok(())
+    }
+    "052" => {
+      solutions::day052::main(data_dir);
+      Ok(())
+    }
+    "061" => {
+      solutions::day061::main(data_dir);
+      Ok(())
+    }
+    "062" => {
+      solutions::day062::main(data_dir);
+      Ok(())
+    }
+    "071" => {
+      solutions::day071::main(data_dir);
+      Ok(())
+    }
+    "072" => {
+      solutions::day072::main(data_dir);
+      Ok(())
+    }
+    "081" => {
+      solutions::day081::main(data_dir);
+      Ok(())
+    }
+    "082" => {
+      solutions::day082::main(data_dir); // Fix the function call
+      Ok(())
+    }
+    "091" => {
+      solutions::day091::main(data_dir);
+      Ok(())
+    }
+    // "092" => {
+    //     solutions::day092::main(data_dir);
+    //     Ok(())
+    // }
+    // "101" => {
+    //     solutions::day101::main(data_dir);
+    //     Ok(())
+    // }
+    // "102" => {
+    //     solutions::day102::main(data_dir);
+    //     Ok(())
+    // }
+    // "111" => {
+    //     solutions::day111::main(data_dir);
+    //     Ok(())
+    // }
+    // "112" => {
+    //     solutions::day112::main(data_dir);
+    //     Ok(())
+    // }
+    // "121" => {
+    //     solutions::day121::main(data_dir);
+    //     Ok(())
+    // }
+    // "122" => {
+    //     solutions::day122::main(data_dir);
+    //     Ok(())
+    // }
+    // "131" => {
+    //     solutions::day131::main(data_dir);
+    //     Ok(())
+    // }
+    // "132" => {
+    //     solutions::day132::main(data_dir);
+    //     Ok(())
+    // }
+    // "141" => {
+    //     solutions::day141::main(data_dir);
+    //     Ok(())
+    // }
+    // "142" => {
+    //     solutions::day142::main(data_dir);
+    //     Ok(())
+    // }
+    // "151" => {
+    //     solutions::day151::main(data_dir);
+    //     Ok(())
+    // }
+    // "152" => {
+    //     solutions::day152::main(data_dir);
+    //     Ok(())
+    // }
+    // "161" => {
+    //     solutions::day161::main(data_dir);
+    //     Ok(())
+    // }
+    // "162" => {
+    //     solutions::day162::main(data_dir);
+    //     Ok(())
+    // }
+    // "171" => {
+    //     solutions::day171::main(data_dir);
+    //     Ok(())
+    // }
+    // "172" => {
+    //     solutions::day172::main(data_dir);
+    //     Ok(())
+    // }
+    // "181" => {
+    //     solutions::day181::main(data_dir);
+    //     Ok(())
+    // }
+    // "182" => {
+    //     solutions::day182::main(data_dir);
+    //     Ok(())
+    // }
+    // "191" => {
+    //     solutions::day191::main(data_dir);
+    //     Ok(())
+    // }
+    // "192" => {
+    //     solutions::day192::main(data_dir);
+    //     Ok(())
+    // }
+    // "201" => {
+    //     solutions::day201::main(data_dir);
+    //     Ok(())
+    // }
+    // "202" => {
+    //     solutions::day202::main(data_dir);
+    //     Ok(())
+    // }
+    // "211" => {
+    //     solutions::day211::main(data_dir);
+    //     Ok(())
+    // }
+    // "212" => {
+    //     solutions::day212::main(data_dir);
+    //     Ok(())
+    // }
+    // "221" => {
+    //     solutions::day221::main(data_dir);
+    //     Ok(())
+    // }
+    // "222" => {
+    //     solutions::day222::main(data_dir);
+    //     Ok(())
+    // }
+    // "231" => {
+    //     solutions::day231::main(data_dir);
+    //     Ok(())
+    // }
+    // "232" => {
+    //     solutions::day232::main(data_dir);
+    //     Ok(())
+    // }
+    // "241" => {
+    //     solutions::day241::main(data_dir);
+    //     Ok(())
+    // }
+    // "242" => {
+    //     solutions::day242::main(data_dir);
+    //     Ok(())
+    // }
+    // "251" => {
+    //     solutions::day251::main(data_dir);
+    //     Ok(())
+    // }
+    // "252" => {
+    //     solutions::day252::main(data_dir);
+    //     Ok(())
+    // }
+
+    // <-- INSERT NEW DAY HERE -->
+    _ => Err(Error::DayNotImplemented(day)),
+  }
 }
 
 // pub fn run_all(data_dir: &str) {
--- src/solutions/day091.rs
+++ src/solutions/day091.rs
@@ -1,76 +1,76 @@
-use crate::{data::load, Error};
+use crate::{ data::load, Error };
 
 #[derive(Error, Debug, PartialEq, Eq)]
 pub enum PuzzleErr {
-    #[error("Could not locate digit.")]
-    NoDigits,
+  #[error("Could not locate digit.")]
+  NoDigits,
 }
 
 // Main puzzle-solving function
 pub fn puzzle(input_data: &str) -> Result<isize, PuzzleErr> {
-    calculate_total(input_data)
+  calculate_total(input_data)
 }
 
 // Function to calculate total winnings
 fn calculate_total(input_data: &str) -> Result<isize, PuzzleErr> {
-    // Computed differences in original 
-    let mut running_total = 0;
+  // Computed differences in original
+  let mut running_total = 0;
 
-    for line in input_data.lines() {
-        let mut start_vec: Vec<isize> = line
-            .split_whitespace()
-            .map(|s| s.parse::<isize>().unwrap_or(0))
-            .collect();
+  for line in input_data.lines() {
+    let mut start_vec: Vec<isize> = line
+      .split_whitespace()
+      .map(|s| s.parse::<isize>().unwrap_or(0))
+      .collect();
 
-        let original_vec = start_vec.clone();
-        let mut difference_vecs: Vec<Vec<isize>> = Vec::new();
-        let mut difference_vec: Vec<isize> = vec![1 as isize];
+    let original_vec = start_vec.clone();
+    let mut difference_vecs: Vec<Vec<isize>> = Vec::new();
+    let mut difference_vec: Vec<isize> = vec![1 as isize];
 
-        while !difference_vec.iter().all(|&x| x == 0) {
-            difference_vec.clear();
-            let mut current_index = 0;
-            while current_index < start_vec.len() - 1 {
-                let diff = start_vec[current_index + 1] - start_vec[current_index];
-                difference_vec.push(diff);
-                current_index += 1;
-            }
-            difference_vecs.push(difference_vec.clone());
-            start_vec = difference_vec.clone();
-        }
-
-        // Now calculate the amount to add to the running total
-        let mut value_to_add_to_total = 0;
-
-        // Iterate through difference_vecs starting from the second-to-last vector
-        for i in (1..difference_vecs.len()).rev() {
-            // Get the last value of the current vector
-            let last_value_current = value_to_add_to_total;
-
-            // Get the last value of the previous vector
-            let last_value_previous = difference_vecs[i - 1].last().cloned().unwrap_or_default();
-
-            // Add the last value of the current vector to the last value of the previous vector
-            value_to_add_to_total = last_value_current + last_value_previous;
-        }
-
-        // Add the last value of the original_vec to the running total
-        value_to_add_to_total += original_vec.last().cloned().unwrap_or_default();
-
-        // Add the calculated value to the running total
-        running_total += value_to_add_to_total;
+    while !difference_vec.iter().all(|&x| x == 0) {
+      difference_vec.clear();
+      let mut current_index = 0;
+      while current_index < start_vec.len() - 1 {
+        let diff = start_vec[current_index + 1] - start_vec[current_index];
+        difference_vec.push(diff);
+        current_index += 1;
+      }
+      difference_vecs.push(difference_vec.clone());
+      start_vec = difference_vec.clone();
     }
 
-    Ok(running_total)
+    // Now calculate the amount to add to the running total
+    let mut value_to_add_to_total = 0;
+
+    // Iterate through difference_vecs starting from the second-to-last vector
+    for i in (1..difference_vecs.len()).rev() {
+      // Get the last value of the current vector
+      let last_value_current = value_to_add_to_total;
+
+      // Get the last value of the previous vector
+      let last_value_previous = difference_vecs[i - 1].last().cloned().unwrap_or_default();
+
+      // Add the last value of the current vector to the last value of the previous vector
+      value_to_add_to_total = last_value_current + last_value_previous;
+    }
+
+    // Add the last value of the original_vec to the running total
+    value_to_add_to_total += original_vec.last().cloned().unwrap_or_default();
+
+    // Add the calculated value to the running total
+    running_total += value_to_add_to_total;
+  }
+
+  Ok(running_total)
 }
 
 // Main function to execute the puzzle
 pub fn main(data_dir: &str) {
-    let data = load(data_dir, 091, None);
+  let data = load(data_dir, 091, None);
 
-    let answer = puzzle(&data);
-    match answer {
-        Ok(x) => println!(" Puzzle 9-1: {}", x),
-        Err(e) => panic!("No solution to puzzle: {}.", e),
-    }
-    assert_eq!(answer, Ok(9064949303801));
+  let answer = puzzle(&data);
+  match answer {
+    Ok(x) => println!(" Puzzle 9-1: {}", x),
+    Err(e) => panic!("No solution to puzzle: {}.", e),
+  }
+  assert_eq!(answer, Ok(1953784198));
 }
